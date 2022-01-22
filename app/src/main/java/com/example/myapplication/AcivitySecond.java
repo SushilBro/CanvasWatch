81bca468afdf0371029e0c61d9b7764b28145a33
Sushil <susheelgautam321@gmail.com>
2022-01-22 20:36:22 +0545

    Canvas Watch Android

---
+++ app/src/main/java/com/example/myapplication/AcivitySecond.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AcivitySecond extends AppCompatActivity {
+BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+}
